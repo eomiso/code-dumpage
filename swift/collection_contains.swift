cf9c2d6409a1e9fa26f5518fffc508c60e735760
Uiseop Eom <ytrtef@gmail.com>
2022-02-12 17:10:32 +0900

    code: Add swift collection tests

---
+++ swift/collection_contains.swift
@@ -0,0 +1,12 @@
+let a = [ 1, 2, 3 ]
+
+print(a.contains(3))
+
+print(a.last)
+
+let b = ["a": 1, "b": 2, "c": 3]
+
+print(b.keys.contains("c"))
+
+print(b.values.contains(3))
+
---
+++ swift/string_iter.swift
@@ -0,0 +1,6 @@
+let a = "this is a string"
+
+for c in a {
+    print(c)
+    print(type(of: c))
+}
